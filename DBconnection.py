f1205ea770e3e05c3c983413ec4104641ad7c5b6
Thinh Nguyen <tnntech@gatech.edu>
2018-11-06 22:08:44 -0500

    Firebase class

---
+++ DBconnection.py
@@ -0,0 +1,28 @@
+import pyrebase, json
+
+
+'''
+Firebase class
+'''
+class Firebase():
+	firebase = None
+	def __init__(self):
+		with open('./firebase/config.json') as json_data:
+			config = json.load(json_data)
+		DBfirebase = pyrebase.initialize_app(config)
+		self.firebase = DBfirebase.database()
+
+	def getAccounts_Firebase(self):
+		return self.firebase.child('accounts').get().val()
+
+	def updateAccount_Firebases(self, fixedData):
+		pass
+
+	def addNewAccount_Firebase(self, newAccount):
+		pass
+
+	def getLocation_Firebase(self):
+		return self.firebase.child('locations').get().val()
+
+	def getDonations_Firebase(self):
+		return self.firebase.child('donations').get().val()
