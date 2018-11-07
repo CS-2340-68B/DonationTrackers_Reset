0acc1bf22e733fd505f674762b331cd231dc1945
Thinh Nguyen <tnntech@gatech.edu>
2018-11-06 22:07:35 -0500

    new logic implemented, including firebase, method POST

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, redirect, session, request, url_for, flash
+from flask import Flask, render_template, redirect, session, request, url_for, flash, abort
+import os, time, json
+import DBconnection as db
 
 app = Flask(__name__)
 
-'''
-Main page index.html
-'''
+# API Call use for firebase
+# https://github.com/thisbejim/Pyrebase
+
+# Connecting to Firebase
+connect = db.Firebase()
+donationList = connect.getDonations_Firebase()
+# print(donationList)
+
+# Main page index.html
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/form", methods=["POST"])
-def resetPass():
-	return
+# Sign in
+@app.route("/signin", methods=["POST"])
+def signin():
+	checkValidUser = True
+	if request.method == 'POST':
+		theEmail = request.form['email_signin']
+		thePassword = request.form['password_signin']
+		return render_template("index.html")
+	else:
+		print(firebase.get('/accounts', None))
+		return render_template("index.html")
 
 # Run server
 app.run(debug=True,host='0.0.0.0', port=5000)
