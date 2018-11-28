632764a3769afbc198f15d580f4f8c0456390b60
Thinh Nguyen <tnntech@gatech.edu>
2018-11-28 03:02:43 -0500

    new view template + static files

---
+++ static/functions/home.js
@@ -0,0 +1,20 @@
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function openToggle() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidebar with the close button
+function closeToggle() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}
