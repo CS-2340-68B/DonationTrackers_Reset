54dd0fca3fb26884d3f679deed1988de6f71cfac
Thinh Nguyen <tnntech@gatech.edu>
2018-12-04 03:42:10 -0500

    new class to store donation item detail

---
+++ Server/Models/ItemDetail.py
@@ -0,0 +1,10 @@
+class ItemDetail:
+    def __init__(self, category, name, shortDes, fullDes, comment, location, time, value):
+        self.category = category
+        self.name = name
+        self.shortDescription = shortDes
+        self.fullDescription = fullDes
+        self.comment = comment
+        self.location = location
+        self.time = time
+        self.value = value
